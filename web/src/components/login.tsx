d807bcafe8ead98a536e819ded28f97f3ddb2438
tamer ugur <tamerugur3@hotmail.com>
2024-10-07 11:19:42 +0300

    added some grid and flex. Trying to implement the logic of the design in my mind.

---
+++ web/src/components/login.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import styles from "./login.module.css";
+
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <div className={styles["form-container"]}>
+        <div className={styles.test}>
+          <div className={styles["test-1"]}></div>
+          <div className={styles["test-2"]}></div>
+          <div className={styles["test-3"]}></div>
+          <div className={styles["test-4"]}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
