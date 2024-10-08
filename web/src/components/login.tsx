3a7d2b5fefa97e468cea5598d6f373d240996c05
tamer ugur <tamerugur3@hotmail.com>
2024-10-08 19:49:17 +0300

    added some border and the red part of the login screen

--- web/src/components/login.tsx
+++ web/src/components/login.tsx
@@ -8,9 +8,15 @@ export default function Login() {
       <div className={styles["form-container"]}>
         <div className={styles.test}>
           <div className={styles["test-1"]}></div>
-          <div className={styles["test-2"]}></div>
+          <div className={styles["test-2"]}>
+            <p>Welcome Back!</p>
+            <p>Enter your details to sign in</p>
+            <p>Don&apos;t have an account?</p>
+            <button>SIGN UP</button>
+          </div>
           <div className={styles["test-3"]}></div>
           <div className={styles["test-4"]}></div>
+          <div className={styles["red-panel"]}></div>
         </div>
       </div>
     </div>
